c57113ca7e981a2b690d8fd2195f4f3e461bd8df
superfeeling-git <58456025+superfeeling-git@users.noreply.github.com>
2021-07-30 23:10:43 +0800

    添加项目文件。

---
+++ quartzService/Job/WriteLogJob.cs
@@ -0,0 +1,43 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quartzService.Job
+{
+    [PersistJobDataAfterExecution]//保存执行状态
+    [DisallowConcurrentExecution]//不允许并发执行
+    class WriteLogJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") +":执行调度");
+            WriteLogFun("");
+            await Task.Delay(1);
+        }
+
+        /// <summary>
+        /// Log 日志
+        /// </summary>
+        /// <param name="logInfo"></param>
+        private void WriteLogFun(string logInfo)
+        {
+            string currentPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string logDirectory = currentPath + "MyServiceLog_" + DateTime.Now.ToString("yyyy");
+            string logFileName = logDirectory + "\\" + DateTime.Today.ToString("yyyyMMdd") + "-1.txt";
+            if (!Directory.Exists(logDirectory))
+            {
+                Directory.CreateDirectory(logDirectory);
+            }
+            using (StreamWriter sw = new StreamWriter(logFileName, true))
+            {
+                sw.WriteLine(System.DateTime.Now.ToString() + " " + logInfo);
+                sw.Flush();
+                sw.Close();
+            }
+        }
+    }
+}
---
+++ quartzService/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Simpl;
+using Quartz.Xml;
+
+namespace quartzService
+{
+    class Program
+    {
+
+        //protected void Application_Start(object sender,EventArgs e)
+        public static void Application_Start()
+        {
+            RunpProgramRunExample().GetAwaiter().GetResult();
+        }
+
+
+        static void Main(string[] args)
+        {
+            Application_Start();
+
+            Console.ReadKey();
+        }
+        private static async Task RunpProgramRunExample()
+        {
+            try
+            {
+                Console.WriteLine("[---------开启调度任务-----------]");
+                
+                XMLSchedulingDataProcessor processor = new XMLSchedulingDataProcessor(new SimpleTypeLoadHelper());
+                StdSchedulerFactory factory = new StdSchedulerFactory();
+                IScheduler scheduler = await factory.GetScheduler();
+                await processor.ProcessFileAndScheduleJobs("~/quartz_jobs.xml", scheduler);
+
+                await scheduler.Start();
+
+                await Task.Delay(TimeSpan.FromSeconds(60));
+
+                await scheduler.Shutdown();  
+            }
+            catch (SchedulerException es)
+            {
+                await Console.Error.WriteLineAsync(es.ToString());
+            }
+        }
+    }
+}
